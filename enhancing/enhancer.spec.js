c46ccc3a08f0a6c60a3e12fa1ba833f72d301521
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-22 14:49:26 -0600

    Complete repair test cases

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,7 +3,17 @@ const enhancer = require("./enhancer.js");
 
 describe("enhancer", () => {
   describe("repair", () => {
-    it.todo("should set object's durability to 100");
+    it("should set object's durability to 100", () => {
+      const item = {
+        name: "Totem of Undying",
+        durability: 4,
+        enchancement: 1,
+      };
+
+      const repaired = enhancer.repair(item);
+
+      expect(repaired.durability).toBe(100);
+    });
   });
   describe("success", () => {
     it.todo("should increase enchancement by one");
@@ -11,8 +21,14 @@ describe("enhancer", () => {
     it.todo("should not affect durability");
   });
   describe("fail", () => {
-    it.todo("should decrease durability by 5 for items less than enhancement 15");
-    it.todo("should decrease durability by 10 for items enhancement 15 or more");
-    it.todo("should decrease enhancement by 1 for items enhancement 16 or more");
+    it.todo(
+      "should decrease durability by 5 for items less than enhancement 15"
+    );
+    it.todo(
+      "should decrease durability by 10 for items enhancement 15 or more"
+    );
+    it.todo(
+      "should decrease enhancement by 1 for items enhancement 16 or more"
+    );
   });
 });
